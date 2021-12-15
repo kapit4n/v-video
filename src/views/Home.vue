27593d4932ea9737182fdf7e18df1dc9152ec737
Luis Arce <luis.arce22@gmail.com>
2021-12-15 19:13:12 -0400

    Init v-video

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Button>Test 123</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+import { Button } from '@/components/Button';
+
+@Options({
+  components: {
+    Button,
+  },
+})
+export default class Home extends Vue {}
+</script>
